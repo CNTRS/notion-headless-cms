107cde166796de388d9759735465672ec8ccdf00
CNTRS <m.contreras.calzon@gmail.com>
2023-09-23 16:01:18 +0200

    Update vite config for build and remove files

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,12 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
     build: {
-        lib: { entry: resolve(__dirname, "src/main.ts"), formats: ["es"] },
+        lib: {
+            entry: resolve(__dirname, "src/main.ts"),
+            formats: ["es"],
+            name: "NotionCMS",
+            fileName: "notion-cms",
+        },
     },
     resolve: { alias: { src: resolve("src/") } },
 });
